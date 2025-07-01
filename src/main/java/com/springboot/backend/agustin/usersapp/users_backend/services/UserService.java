073e81f4ae1168ea9c9bee888a0f91061d28869d
Agustin <agustingh1583@gmail.com>
2025-07-01 16:58:24 -0500

    3ra

--- src/main/java/com/springboot/backend/agustin/usersapp/users_backend/services/UserService.java
+++ src/main/java/com/springboot/backend/agustin/usersapp/users_backend/services/UserService.java
@@ -3,13 +3,15 @@ package com.springboot.backend.agustin.usersapp.users_backend.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.lang.NonNull;
+
 import com.springboot.backend.agustin.usersapp.users_backend.entities.User;
 
 public interface UserService {
 
     List<User> findAll();
 
-    Optional<User> findById(Long id);
+    Optional<User> findById(@NonNull Long id);
 
     User save(User user);
 
--- src/main/java/com/springboot/backend/agustin/usersapp/users_backend/services/UserServiceImpl.java
+++ src/main/java/com/springboot/backend/agustin/usersapp/users_backend/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,25 +20,25 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional(readOnly = true)
     public List<User> findAll() {
-
+        return (List)this.repository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
-    public Optional<User> findById(Long id) {
-      
+    public Optional<User> findById(@NonNull Long id) {
+        return this.repository.findById(id);
     }
 
     @Override
     @Transactional
     public User save(User user) {
-      
+      return repository.save(user);
     }
 
     @Override
     @Transactional
     public void deleteById(Long id) {
-       
+       repository.deleteById(id);
     }
 
 }
