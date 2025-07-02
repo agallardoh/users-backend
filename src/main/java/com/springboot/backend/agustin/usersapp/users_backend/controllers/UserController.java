20ad3114546471ba59a5df387a9ec36f6d2b7880
Agustin <agustingh1583@gmail.com>
2025-07-02 14:38:57 -0500

    implementacion de put y post para user

--- src/main/java/com/springboot/backend/agustin/usersapp/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/agustin/usersapp/users_backend/controllers/UserController.java
@@ -11,13 +11,20 @@ import com.springboot.backend.agustin.usersapp.users_backend.services.UserServic
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -36,5 +43,36 @@ public class UserController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("error", "el usuario no se encontro por el id:" + id));
     }
+
+    @PostMapping
+    public ResponseEntity<User> create (@RequestBody User user)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        
+        Optional<User> userOptional = service.findById(id);
+        if(userOptional.isPresent()){
+            User userBd = userOptional.get();
+            userBd.setEmail(user.getEmail());
+            userBd.setLastname(user.getLastname());
+            userBd.setName(user.getName());
+            userBd.setPassword(user.getPassword());
+            userBd.setUsername(user.getUsername());
+            return ResponseEntity.ok(service.save(userBd));
+        }
+        return ResponseEntity.notFound().build();
+    }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Optional<User> userOptional = service.findById(id);
+        if(userOptional.isPresent()){
+            service.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
